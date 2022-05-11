f852a6365d85e5d792231e47f2c3449a1f081801
Louisa Bieler <louisa.lynne.bieler@gmail.com>
2022-05-11 13:51:35 +0200

    changed to show function calls in docstrings

--- load_transform.py
+++ load_transform.py
@@ -14,30 +14,28 @@ import sklearn
 
 # load and explore:
 def load_explore(csv_file):
+    """to use this function, call as follows: df = load_explore('train.csv')"""
     df = pd.read_csv(csv_file, index_col='PassengerId')
     print(df.head(), df.shape, df.dtypes, df.describe(), df.isna().sum() * 100 / len(df))
     return df
 
-df = load_explore('train.csv')
 
 
 # Transform by making a gender boolean column:
-
 def gender_boolean(row):
-        if 'female' in str(row):
-            return 1
-        else:
-            return 0
+    """to use this function, call as follows: df['GenderBoolean'] = df['Sex'].apply(gender_boolean)"""
+    if 'female' in str(row):
+        return 1
+    else:
+        return 0   
 
-df['GenderBoolean'] = df['Sex'].apply(gender_boolean)
     
     
 def create_clean_df(df, columns_to_keep):
+    """to use this function, call as follows: clean_df = create_clean_df(df, ['Survived', 'Pclass', 'Age', 'SibSp', 'GenderBoolean'])"""
         clean_df = pd.DataFrame()
         for i in columns_to_keep:
             clean_df[i] = df[[i]]
         return clean_df    
     
-clean_df = create_clean_df(df, ['Survived', 'Pclass', 'Age', 'SibSp', 'GenderBoolean'])
-print(clean_df.head())
     
