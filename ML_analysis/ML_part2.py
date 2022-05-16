e0cc03da7e3342b2dd40a11595da0c1400cab9ec
Louisa Bieler <louisa.lynne.bieler@gmail.com>
2022-05-16 16:05:08 +0200

    fixes

--- ML_analysis/ML_part2.py
+++ ML_analysis/ML_part2.py
@@ -1,3 +1,24 @@
+### Import packages
+# linear algebra
+import numpy as np 
+# data processing
+import pandas as pd 
+from sklearn.impute import SimpleImputer
+# data visualization
+import seaborn as sns
+from matplotlib import pyplot as plt
+from matplotlib import style
+# Algorithms
+import sklearn as sk
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, GridSearchCV
+
+
+
 def split_set(df, size):
     """
     df: Dataframe to split
@@ -5,32 +26,39 @@ def split_set(df, size):
     """
     return train_test_split(df[['Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'not_alone','Sex_female','Embarked_Q','Embarked_S']], df['Survived'], test_size=size)
 
+
 def predict_kNN(dataset, min_neighbors= 1, max_neighbors = 15, cross_val_folds=5):
     """
     k-NN algoritm and finding the best parameters for it
     """
+    from sklearn.metrics import classification_report, confusion_matrix
+    X_train, X_test, y_train, y_test = split_set(dataset, 0.3)
     param_grid = {'n_neighbors': np.arange(min_neighbors, max_neighbors)}
-    knn = KNeighborsClassifier()
-    knn_cv = GridSearchCV(knn, param_grid, cv=cross_val_folds)
-    knn_cv.fit(X_train, y_train)
-    y_pred = knn_cv.predict(X_test)
-    print(knn_cv.best_params_)
-    print(knn_cv.best_score_)
+    sk.knn = KNeighborsClassifier()
+    sk.knn_cv = GridSearchCV(sk.knn, param_grid, cv=cross_val_folds)
+    sk.knn_cv.fit(X_train, y_train)
+    y_pred = sk.knn_cv.predict(X_test)
+    print(sk.knn_cv.best_params_)
+    print(sk.knn_cv.best_score_)
     print(classification_report(y_test,y_pred))
     knn_classreport= classification_report(y_test,y_pred)
     return knn_classreport
 
 def predict_logreg(dataset):
     """Logistic Regression algorithm"""
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.metrics import classification_report, confusion_matrix
+    X_train, X_test, y_train, y_test = split_set(dataset, 0.3)
     logreg=LogisticRegression()
     logreg.fit(X_train, y_train)
     y_pred=logreg.predict(X_test)
     print(classification_report(y_test,y_pred))
-    logreg_classresport=classification_report(y_test,y_pred)
-    return log_classreport
+    logreg_classreport = classification_report(y_test,y_pred)
+    return logreg_classreport
 
 def predict_RF(dataset):
     """Random Forest  - finding the best hyperparameters"""
+    X_train, X_test, y_train, y_test = split_set(dataset, 0.3)
     rf=RandomForestClassifier()
     params_rf={'criterion':['gini','entropy'],'n_estimators':[100,200], 'max_depth':[3,4,5], 'min_samples_leaf':[1,2,3,5],'min_samples_split':[2,3,4,5], 'max_features':['auto','log2']}
     grid_rf=GridSearchCV(estimator=rf, param_grid=params_rf,cv=10, n_jobs=-1)
@@ -41,6 +69,7 @@ def predict_RF(dataset):
     return best_hyper_rf
 
 def bestRF(dataset):
+    X_train, X_test, y_train, y_test = split_set(dataset, 0.3)
     """Running Random Forest algorithm with the best hyperparameters"""
     rf_bestmodel=RandomForestClassifier(criterion='entropy', max_depth=3, max_features='auto', min_samples_leaf=3, min_samples_split=5, n_estimators=100, class_weight='balanced')
     rf_bestmodel.fit(X_train,y_train)
---
+++ load_transform.py
@@ -0,0 +1,39 @@
+# Load, Explore, Clean/Transform
+
+# imports:
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib_inline
+import seaborn as sns
+# set the color palette
+sns.set_palette(sns.color_palette("husl"))
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+import sklearn
+
+
+# load and explore:
+def load_explore(csv_file):
+    """to use this function, call as follows: df = load_explore('train.csv')"""
+    df = pd.read_csv(csv_file, index_col='PassengerId')
+    df.head(), 
+    return df
+
+
+
+# Transform by making a gender boolean column:
+def gender_boolean(row):
+    """to use this function, call as follows: df['GenderBoolean'] = df['Sex'].apply(gender_boolean)"""
+    if 'female' in str(row):
+        return 1
+    else:
+        return 0   
+
+    
+    
+def create_clean_df(df, columns_to_keep):
+    """to use this function, call as follows: clean_df = create_clean_df(df, ['Survived', 'Pclass', 'Age', 'SibSp', 'GenderBoolean'])"""
+    clean_df = pd.DataFrame()
+    for i in columns_to_keep:
+        clean_df[i] = df[[i]]
+        return clean_df
